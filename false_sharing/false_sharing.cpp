8c49675fb8e74f757b82365f442ac5776ed30dc9
Nick <CoffeeBeforeArch@gmail.com>
2019-12-19 22:35:28 -0500

    redo false sharing to be an actual benchmark

--- false_sharing/false_sharing.cpp
+++ false_sharing/false_sharing.cpp
@@ -1,22 +1,20 @@
 // This program shows off the sever implications of false sharing in
 // C++ using std::atomic and std::thread
 
+#include <benchmark/benchmark.h>
 #include <atomic>
-#include <iostream>
 #include <thread>
 
-using namespace std;
-
 // Simple function for incrememnting an atomic int
-void work(atomic<int>& a) {
-  for (int i = 0; i < 10000; i++) {
+void work(std::atomic<int>& a) {
+  for (int i = 0; i < 100000; i++) {
     a++;
   }
 }
 
 // Simple single-threaded function that calls work 4 times
 void single_thread() {
-  atomic<int> a;
+  std::atomic<int> a;
   a = 0;
 
   work(a);
@@ -25,18 +23,26 @@ void single_thread() {
   work(a);
 }
 
+// A simple benchmark that runs our single-threaded implementation
+static void singleThread(benchmark::State& s) {
+  while (s.KeepRunning()) {
+    single_thread();
+  }
+}
+BENCHMARK(singleThread)->Unit(benchmark::kMillisecond);
+
 // Tries to parallelize the work across multiple threads
 // However, each core invalidates the other cores copies on a write
 // This is an EXTREME example of poorly thought out code
 void same_var() {
-  atomic<int> a;
+  std::atomic<int> a;
   a = 0;
 
   // Create four threads and use a lambda to launch work
-  thread t1([&]() { work(a); });
-  thread t2([&]() { work(a); });
-  thread t3([&]() { work(a); });
-  thread t4([&]() { work(a); });
+  std::thread t1([&]() { work(a); });
+  std::thread t2([&]() { work(a); });
+  std::thread t3([&]() { work(a); });
+  std::thread t4([&]() { work(a); });
 
   // Join the threads
   t1.join();
@@ -44,6 +50,14 @@ void same_var() {
   t3.join();
   t4.join();
 }
+
+// A simple benchmark that runs our single-threaded implementation
+static void directSharing(benchmark::State& s) {
+  while (s.KeepRunning()) {
+    same_var();
+  }
+}
+BENCHMARK(directSharing)->UseRealTime()->Unit(benchmark::kMillisecond);
 
 // How well does it work if we use different atomic ints?
 // Not that well! But look at the addresses! They all reside on the
@@ -51,24 +65,16 @@ void same_var() {
 // (We invalidate variables that aren't actually being accessed
 // because they happen to be on the same cache line)
 void diff_var() {
-  atomic<int> a;
-  a = 0;
-  cout << "Address of a: " << &a << endl;
-  atomic<int> b;
-  b = 0;
-  cout << "Address of b: " << &b << endl;
-  atomic<int> c;
-  c = 0;
-  cout << "Address of c: " << &c << endl;
-  atomic<int> d;
-  d = 0;
-  cout << "Address of d: " << &d << endl;
+  std::atomic<int> a{0};
+  std::atomic<int> b{0};
+  std::atomic<int> c{0};
+  std::atomic<int> d{0};
 
   // Creat four threads and use lambda to launch work
-  thread t1([&]() { work(a); });
-  thread t2([&]() { work(b); });
-  thread t3([&]() { work(c); });
-  thread t4([&]() { work(d); });
+  std::thread t1([&]() { work(a); });
+  std::thread t2([&]() { work(b); });
+  std::thread t3([&]() { work(c); });
+  std::thread t4([&]() { work(d); });
 
   // Join the threads
   t1.join();
@@ -77,32 +83,33 @@ void diff_var() {
   t4.join();
 }
 
-// Simple fix, just make sure we can't get two atomics on the same
-// cache line
-struct alignas(64) aligned_type {
-  atomic<int> a;
+// A simple benchmark that runs our single-threaded implementation
+static void falseSharing(benchmark::State& s) {
+  while (s.KeepRunning()) {
+    diff_var();
+  }
+}
+BENCHMARK(falseSharing)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+// We can align the struct to 64 bytes
+// Now each struct will be on a different cache line
+struct alignas(64) AlignedType {
+  AlignedType() { val = 0; }
+  std::atomic<int> val;
 };
 
 // No more invalidations, so our code should be roughly the same as the
 void diff_line() {
-  aligned_type a;
-  a.a = 0;
-  cout << "Address of a: " << &a << endl;
-  aligned_type b;
-  b.a = 0;
-  cout << "Address of b: " << &b << endl;
-  aligned_type c;
-  c.a = 0;
-  cout << "Address of c: " << &c << endl;
-  aligned_type d;
-  d.a = 0;
-  cout << "Address of d: " << &d << endl;
+  AlignedType a{};
+  AlignedType b{};
+  AlignedType c{};
+  AlignedType d{};
 
   // Launch the four threads now using our aligned data
-  thread t1([&]() { work(a.a); });
-  thread t2([&]() { work(b.a); });
-  thread t3([&]() { work(c.a); });
-  thread t4([&]() { work(d.a); });
+  std::thread t1([&]() { work(a.val); });
+  std::thread t2([&]() { work(b.val); });
+  std::thread t3([&]() { work(c.val); });
+  std::thread t4([&]() { work(d.val); });
 
   // Join the threads
   t1.join();
@@ -111,10 +118,12 @@ void diff_line() {
   t4.join();
 }
 
-int main() {
-  single_thread();
-  // same_var();
-  // diff_var();
-  // diff_line();
-  return 0;
+// A simple benchmark that runs our single-threaded implementation
+static void noSharing(benchmark::State& s) {
+  while (s.KeepRunning()) {
+    diff_line();
+  }
 }
+BENCHMARK(noSharing)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
