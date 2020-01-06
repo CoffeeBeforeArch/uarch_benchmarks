de6108e58c78f8958f818f48bb463267cd64f9ec
Nick <CoffeeBeforeArch@gmail.com>
2020-01-06 18:17:40 -0500

    start re-writing the cache associativity example

--- associativity/associativity.cpp
+++ associativity/associativity.cpp
@@ -3,38 +3,40 @@
 
 #include <benchmark/benchmark.h>
 #include <algorithm>
-#include <array>
 #include <cstdlib>
+#include <vector>
 
-using std::array;
 using std::generate;
+using std::vector;
 
 // Benchmark for showing cache associativity
 static void assocBench(benchmark::State &s) {
   // Use a variable step size (power of 2)
   int step = 1 << s.range(0);
 
-  // Size of the array is constant (32MB)
-  const int N = 1 << 25;
-  array<int, N> a;
+  // 32 MB vector
+  const int N = 1 << 23;
+  vector<int> v(N);
 
-  // Initialize the array with some random numbers
-  generate(begin(a), end(a), []() { return rand() % 100; });
+  // Number of accesses
+  const int MAX_ITER = 1 << 20;
 
   // Profile the runtime of different step sizes
   while (s.KeepRunning()) {
-    int i = 0;
     // Perform 10k accesses
-    for (int iter = 0; iter < 10000; iter++) {
+    int i = 0;
+    for (int iter = 0; iter < MAX_ITER; iter++) {
       // Just increment this int
-      a[i]++;
-      // Wrap back around to the beginning of the array
-      i = (i + step) % N;
+      v[i]++;
+
+      // Reset if we go off the end of the array
+      i += step;
+      if (i >= N) i = 0;
     }
   }
 }
 // Register the benchmark
-BENCHMARK(assocBench)->DenseRange(0, 16)->Unit(benchmark::kMicrosecond);
+BENCHMARK(assocBench)->DenseRange(0, 9)->Unit(benchmark::kMillisecond);
 
 // Benchmark main function
 BENCHMARK_MAIN();
