b587886915f1901929ec1622bbcbea5e3b9135d9
Nick <CoffeeBeforeArch@gmail.com>
2020-01-08 16:52:13 -0500

    expand on polymorphism example

--- branch_prediction/vf_calls.cpp
+++ branch_prediction/vf_calls.cpp
@@ -3,12 +3,11 @@
 
 #include <benchmark/benchmark.h>
 #include <algorithm>
-#include <iterator>
 #include <random>
 #include <vector>
 
-using namespace std;
-
+// A simple case of polymorphism
+// One base class with a single virtual function
 struct Mammal {
   virtual float getSomeNumber() const noexcept { return 1.0; }
 };
@@ -21,14 +20,15 @@ struct Cat : Mammal {
   float getSomeNumber() const noexcept { return 3.0; }
 };
 
+// Benchmark where all same-type objects are grouped together
 static void vf_sorted(benchmark::State& s) {
   // Create a vector to store mammals
-  vector<Mammal*> zoo;
+  std::vector<Mammal*> zoo;
 
   // Use fill_n to insert many instances into the vector
-  fill_n(back_inserter(zoo), 10000, new Mammal);
-  fill_n(back_inserter(zoo), 10000, new Dog);
-  fill_n(back_inserter(zoo), 10000, new Cat);
+  std::fill_n(std::back_inserter(zoo), 10000, new Mammal);
+  std::fill_n(std::back_inserter(zoo), 10000, new Dog);
+  std::fill_n(std::back_inserter(zoo), 10000, new Cat);
 
   // Acculate a sum here
   float sum = 0;
@@ -44,26 +44,27 @@ static void vf_sorted(benchmark::State& s) {
 }
 BENCHMARK(vf_sorted);
 
+// Benchmark where ordering of types is randomized
 static void vf_unsorted(benchmark::State& s) {
   // Create a vector to store mammals
-  vector<Mammal*> zoo;
+  std::vector<Mammal*> zoo;
 
   // Use fill_n to insert many instances into the vector
-  fill_n(back_inserter(zoo), 10000, new Mammal);
-  fill_n(back_inserter(zoo), 10000, new Dog);
-  fill_n(back_inserter(zoo), 10000, new Cat);
+  std::fill_n(std::back_inserter(zoo), 10000, new Mammal);
+  std::fill_n(std::back_inserter(zoo), 10000, new Dog);
+  std::fill_n(std::back_inserter(zoo), 10000, new Cat);
 
   // Now shuffle the vector
-  random_device rng;
-  mt19937 urng(rng());
-  shuffle(zoo.begin(), zoo.end(), urng);
+  std::random_device rng;
+  std::mt19937 urng(rng());
+  std::shuffle(zoo.begin(), zoo.end(), urng);
 
   // Acculate a sum here
   float sum = 0;
 
   // Profile here
   while (s.KeepRunning()) {
-    // VF Calls here are ~random, so the branch predicter will have
+    // VF Calls here are ~random, so the branch predictor will have
     // some trouble
     for (auto* animal : zoo) {
       sum += animal->getSomeNumber();
@@ -72,6 +73,33 @@ static void vf_unsorted(benchmark::State& s) {
 }
 // Register the benchmark
 BENCHMARK(vf_unsorted);
+
+// Benchmark where ordering of types is striped
+static void vf_striped(benchmark::State& s) {
+  // Create a vector to store mammals
+  std::vector<Mammal*> zoo;
+  zoo.reserve(30000);
+
+  // Fill the vector with groups of three objects
+  for (int i = 0; i < 10000; i++) {
+    zoo.emplace_back(new Mammal);
+    zoo.emplace_back(new Dog);
+    zoo.emplace_back(new Cat);
+  }
+
+  // Acculate a sum here
+  float sum = 0;
+
+  // Profile here
+  while (s.KeepRunning()) {
+    // VF calls occur in a pattern that is easy to predict
+    for (auto* animal : zoo) {
+      sum += animal->getSomeNumber();
+    }
+  }
+}
+// Register the benchmark
+BENCHMARK(vf_striped);
 
 // Main function
 BENCHMARK_MAIN();
